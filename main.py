9f3a48468239420530fc0c105c1cc56668cf9043
JavohirJalilov <javohirjaliloff@gmail.com>
2023-02-28 19:01:28 +0500

    like and dislike with inline keyboard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, CallbackQueryHandler
 import os 
 from db import LikeDB
 
@@ -11,40 +11,51 @@ def start(update: Update, context: CallbackContext) -> None:
     bot = context.bot
     chat_id = update.message.chat.id
 
-    user_data = likeDB.add_student(str(chat_id))
-    like = user_data['like']
-    dislike = user_data['dislike']
-    btn1 = KeyboardButton(text=f'\U0001F44D {like}')
-    btn2 = KeyboardButton(text=f'\U0001F44E {dislike}')
+    likeDB.add_student(str(chat_id))
+    bot.sendMessage(chat_id,'Sen me picture!')
 
-    keyboard = ReplyKeyboardMarkup([[btn1, btn2]])
-    bot.send_message(chat_id, f"LIKE:{like}  DISLIKE:{dislike}", reply_markup=keyboard)
+def photo(update: Update, context: CallbackContext):
+    bot = context.bot
+    chat_id = update.message.chat.id
+    photo = update.message.photo[-1]["file_id"]
+
+    
+    like = likeDB.all_likes(str(chat_id))
+    dislike = likeDB.all_dislikes(str(chat_id))
+    btn1 = InlineKeyboardButton(text=f'\U0001F44D {like}', callback_data="like")
+    btn2 = InlineKeyboardButton(text=f'\U0001F44E {dislike}', callback_data="dislike")
+
+    keyboard = InlineKeyboardMarkup([[btn1, btn2]])
+    bot.sendPhoto(chat_id=chat_id, photo=photo, reply_markup=keyboard)
 
 def count_like_dislike(update, context):
-    bot = context.bot
+    query = update.callback_query
+    data = query.data
+    
+    chat_id = query.message.chat.id
 
-    chat_id = update.message.chat.id
-    text = update.message.text
-
-    if text[0] == "\U0001F44D":
+    if data == 'like':
         user_data = likeDB.add_like(str(chat_id))
     
-    elif text[0] == "\U0001F44E":
+    elif data == 'dislike':
         user_data = likeDB.add_dislike(str(chat_id))
 
     like = user_data['like']
     dislike = user_data['dislike']
-    btn1 = KeyboardButton(text=f'\U0001F44D {like}')
-    btn2 = KeyboardButton(text=f'\U0001F44E {dislike}')
 
-    keyboard = ReplyKeyboardMarkup([[btn1, btn2]])
-    bot.send_message(chat_id, f"LIKE:{like}  DISLIKE:{dislike}", reply_markup=keyboard)
+    btn1 = InlineKeyboardButton(text=f'\U0001F44D {like}', callback_data="like")
+    btn2 = InlineKeyboardButton(text=f'\U0001F44E {dislike}', callback_data='dislike')
+
+    query.answer(text='✅😂', show_alert=True)
+    keyboard = InlineKeyboardMarkup([[btn1, btn2]])
+    query.edit_message_reply_markup(reply_markup=keyboard)
 
 updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler("start", start))
-dp.add_handler(MessageHandler(Filters.text, count_like_dislike))
+dp.add_handler(MessageHandler(Filters.photo, photo))
+dp.add_handler(CallbackQueryHandler(count_like_dislike))
 
 updater.start_polling()
 updater.idle()
